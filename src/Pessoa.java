4edfc0bf87a75c2be8d66a10d404fff6428b127a
AndreyCsilva2006 <csilvaandrey2006@gmail.com>
2023-06-01 11:06:12 -0300

    Atividade Balonismo 01/06/2023

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,11 +1,11 @@
-public class Pessoa {
+public class Pessoa{
     // Atributos da Classe
-    public String nome;
-    public String CPF;
-    public String RG;
-    public String telefone;
-    public String telefoneFamiliar;
-    public String endereço;
+   // public String nome;
+    //public String CPF;
+   // public String RG;
+    //public String telefone;
+    //public String telefoneFamiliar;
+    //public String endereço;
     // Métodos da Classe
     // Sobrescrito do Método
 }
